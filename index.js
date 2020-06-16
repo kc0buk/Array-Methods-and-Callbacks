93f49df2ba1c923e07ddad3f2bf14f4301f7cd64
Jeremiah Trnka <jeremiah@jeremiahs-mbp.lan>
2020-06-15 17:47:25 -0700

    Updated code for Task 1 in index.js.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { fifaData } from './fifa.js';
-console.log(fifaData);
+// console.log(fifaData);
 
-console.log('its working');
+// console.log('its working');
 // ⚽️ M  V P ⚽️ //
 
 /* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data 
@@ -11,7 +11,39 @@ console.log('its working');
 (c) Home Team goals for 2014 world cup final
 (d) Away Team goals for 2014 world cup final
 (e) Winner of 2014 world cup final */
+console.log(`*************** TASK 1 ***************`);
 
+console.log(fifaData.forEach(function(item) {
+    if (item['Year'] === 2014 && item['Stage'] === 'Final') {
+                console.log(`(a). ${item['Home Team Name']}`);
+            }
+        }));
+
+console.log(fifaData.forEach(function(item) {
+    if (item['Year'] === 2014 && item['Stage'] === 'Final') {
+                console.log(`(b). ${item['Away Team Name']}`);
+            }
+        }));
+
+console.log(fifaData.forEach(function(item) {
+    if (item['Year'] === 2014 && item['Stage'] === 'Final') {
+                console.log(`(c). ${item['Home Team Goals']}`);
+            }
+        }));
+
+console.log(fifaData.forEach(function(item) {
+    if (item['Year'] === 2014 && item['Stage'] === 'Final') {
+                console.log(`(d). ${item['Away Team Goals']}`);
+            }
+        }));
+
+console.log(fifaData.forEach(function(item) {
+    if (item['Year'] === 2014 && item['Stage'] === 'Final' && item['Home Team Goals'] > item['Away Team Goals']) {
+                console.log(`(e). ${item['Home Team Name']} won the 2014 World Cup Final`);
+            } else if (item['Year'] === 2014 && item['Stage'] === 'Final') {
+                console.log(`${item['Away Team Name']} won the 2014 World Cup Final`);
+            }
+        }));
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
